091b55ba0bc7504e965a874334bad151a3a507ba
Pobitrodeb <pobitrodn@gmail.com>
2023-02-06 21:22:27 +0600

    user register crate successfully

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use Validator;
+use Dotenv\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'name'       => 'required',
+            'email'      => 'required|email',
+            'password'   => 'required',
+            'c_password' => 'required|same:password'
+        ]);
+
+        if($validator->fails()){
+            return $this->senError('Validator Error', $validator->errors());
+        }
+
+        $input                      = $request->all();
+        $input['password']          = bcrypt($input['password']);
+        $user                       = User::crate($input);
+
+        // For Message
+        $success['token']           = $user->createToken('MyApp');
+        $success['name']            = $user->name;
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+}
